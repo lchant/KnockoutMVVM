15f61bb675ef1bba273bb947fd3257f038e60d1c
lchant <lchant@hearst.com>
2018-08-21 10:03:00 +0100

    Resolved formatting issues

--- KnockoutMVVM/Scripts/utils.js
+++ KnockoutMVVM/Scripts/utils.js
@@ -1,6 +1,6 @@
-﻿ko.utils.stringStartsWith = function (string, startsWith) {        	
-            string = string || "";
-            if (startsWith.length > string.length)
-                return false;
-            return string.substring(0, startsWith.length) === startsWith;
+﻿ko.utils.stringStartsWith = function (string, startsWith) {
+    string = string || "";
+    if (startsWith.length > string.length)
+        return false;
+    return string.substring(0, startsWith.length) === startsWith;
 };
